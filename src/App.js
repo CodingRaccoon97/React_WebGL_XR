e460f1a227393674a9d719746c84564947726142
CodingRaccoon97 <ghd2tjd8@naver.com>
2022-05-25 13:04:50 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Controls from "./components/Controls";
+
+class App extends Component {
+	render() {
+		return <Controls />;
+	}
+}
+
+export default App;
